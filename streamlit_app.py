f8ff18d2bfb1707711323884a48df421835fd54d
muwenh <mh2038@georgetown.edu>
2022-07-26 12:37:04 -0400

    modified back

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import mysql.connector
 
 # Initialize connection.
 # Uses st.experimental_singleton to only run once.
-#@st.experimental_singleton
+@st.experimental_singleton
 def init_connection():
     return mysql.connector.connect(**st.secrets["mysql"])
 
@@ -11,7 +11,7 @@ conn = init_connection()
 
 # Perform query.
 # Uses st.experimental_memo to only rerun when the query changes or after 10 min.
-#@st.experimental_memo(ttl=600)
+@st.experimental_memo(ttl=600)
 def run_query(query):
     with conn.cursor() as cur:
         cur.execute(query)
